5fea70a0edc5266bfaeac458001f331bf19247c7
asmina18 <asmina18@mail.ru>
2024-02-08 13:57:12 +0100

    Commentary on the homepage is complete.

--- app/src/pages/HomePage/HomePage.jsx
+++ app/src/pages/HomePage/HomePage.jsx
@@ -5,28 +5,35 @@ import style from "./homepage.module.scss"
 
 
 export function HomePage() {
+    // ​‌‍‌⁡⁢⁣⁣Opretter en state variabel til at opbevare data hentet fra serveren.⁡​
     const [data, setData] = useState([]);
-
+    // ​‌‍‌⁡⁢⁣⁣Bruger useEffect til at hente data én gang efter komponenten er indlæst.⁡​
     useEffect(() => {
+        // ​‌‍‌⁡⁢⁣⁣Henter data fra en API.⁡​
         fetch('http://localhost:4000/news')
-            .then(res => res.json())
-            .then(data => setData(data)) 
-            .catch(err => console.error(" error", err)); 
+            .then(res => res.json()) //​‌‍‌⁡⁢⁣⁣ Konverterer svaret til JSON-format.⁡​
+            .then(data => {
+                console.log('Data modtaget:', data);
+                setData(data); // ​‌‍‌⁡⁢⁣⁣Opdaterer state med de nye data.⁡​
+            })
+            .catch(err => console.error("Fejl ved indhentning af data:", err));
     }, []);
 
     return (
         <>
-              
-
             <Title title="Velkommen til overlook online " />
             <div className={style.cardCantainer}>
+               ​‌‍‌⁡⁢⁣⁣ {/* Løber gennem de første tre elementer i `data`-arrayet for at skabe et kort til hver */}⁡​
                 {data && data.slice(0, 3).map((data) => (
                     <figure key={data.id} className={style.hotelCard}>
-                        <img src={`http://localhost:4000/images/${data.image.filename}`} alt="image" style={{width:"50%"}} />
+                        <img src={`http://localhost:4000/images/${data.image.filename}`}
+                         alt="image"
+                          />
                         <figcaption>
+                      ⁡⁢⁣⁣   ​‌‍‌{/* Anvender `figcaption` til at inkludere et link, der detaljeret beskriver nyheden, med nyhedens titel som klikbart element */}​⁡⁡
                             <Link to={`/room`}>
                                 <h4>{data.title}</h4>
-                                <p>{data.teaser}</p>
+                                {/* <p>{data.teaser}</p> */}
                             </Link>
                         </figcaption>
                     </figure>
